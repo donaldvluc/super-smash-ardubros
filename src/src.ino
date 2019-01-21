2a203d8b8f83f4ff090376e60a594fcda0452263
Anthony Luc <anthonyvanluc@gmail.com>
2019-01-21 00:05:34 -0500

    update src and add arduboy tones test

--- src/src.ino
+++ src/src.ino
@@ -11,10 +11,28 @@ License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
 */
 
-#include "Arduboy.h"
+#include "Arduboy2.h"
+#include <ArduboyTones.h>
 
 // make an instance of arduboy used for many functions
-Arduboy arduboy;
+Arduboy2 arduboy;
+ArduboyTones sound(arduboy.audio.enabled);
+
+const uint16_t score[] PROGMEM = {
+ NOTE_G5,100, NOTE_C5,160, NOTE_REST,283, NOTE_G5,4, NOTE_G5,595, NOTE_G5,11, NOTE_REST,1788, NOTE_G5,35,
+ NOTE_G5,864, NOTE_G5,16, NOTE_REST,283, NOTE_G5,4, NOTE_G5,595, NOTE_G5,11, NOTE_REST,588, NOTE_B4,600,
+ NOTE_A4,508, NOTE_A4,91, NOTE_G4,1200, NOTE_G4,600, NOTE_G4,11, NOTE_G4,1188, NOTE_G4,600, NOTE_C5,35,
+ NOTE_C5,1164, NOTE_C5,600, NOTE_C5,11, NOTE_C5,1696, NOTE_C5,91, NOTE_F4,1200, NOTE_F4,600, NOTE_F4,11,
+ NOTE_F4,888, NOTE_F4,16, NOTE_F4,283, NOTE_F4,4, NOTE_F4,595, NOTE_C5,11, NOTE_C5,888, NOTE_C5,16,
+ NOTE_C5,283, NOTE_C5,4, NOTE_C5,595, NOTE_C5,11, NOTE_C5,1696, NOTE_C5,91, NOTE_F4,1200, NOTE_F4,600,
+ NOTE_F4,11, NOTE_F4,888, NOTE_F4,16, NOTE_F4,283, NOTE_F4,4, NOTE_F4,595, NOTE_C5,11, NOTE_C5,888,
+ NOTE_C5,16, NOTE_C5,283, NOTE_C5,4, NOTE_C5,595, NOTE_C5,11, NOTE_C5,588, NOTE_B4,600, NOTE_A4,508,
+ NOTE_A4,91, NOTE_G4,1200, NOTE_G4,600, NOTE_G4,11, NOTE_G4,588, NOTE_G4,11, NOTE_G4,588, NOTE_G4,11,
+ NOTE_G4,588, NOTE_C5,11, NOTE_C5,1788, NOTE_C5,35, NOTE_C5,1673, NOTE_C5,91, NOTE_C5,600, NOTE_C5,11,
+ NOTE_C5,588, NOTE_C5,11, NOTE_C5,588, NOTE_G4,11, NOTE_G4,888, NOTE_G4,16, NOTE_G4,283, NOTE_G4,4,
+ NOTE_G4,595, NOTE_C5,11, NOTE_C5,1696, NOTE_REST,91,
+ TONES_END
+};
 
 
 // This function runs once in your game.
@@ -48,4 +66,7 @@ void loop() {
 
   // then we finaly we tell the arduboy to display what we just wrote to the display
   arduboy.display();
+
+  sound.tones(score);
+
 }
