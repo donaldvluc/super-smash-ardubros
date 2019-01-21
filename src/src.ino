6f41173dee46ab88fc3cfd129f3c2cdd9e2b14ab
Michael <mwang6@nd.edu>
2019-01-21 00:41:07 -0500

    add theme song

--- src/src.ino
+++ src/src.ino
@@ -1,72 +1,330 @@
-/*
-Hello, World! example
-June 11, 2015
-Copyright (C) 2015 David Martinez
-All rights reserved.
-This code is the most basic barebones code for writing a program for Arduboy.
+#include "Arduboy.h"
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-*/
+Arduboy arduboy;
 
-#include "Arduboy2.h"
-#include <ArduboyTones.h>
-
-// make an instance of arduboy used for many functions
-Arduboy2 arduboy;
-ArduboyTones sound(arduboy.audio.enabled);
-
-const uint16_t score[] PROGMEM = {
- NOTE_G5,100, NOTE_C5,160, NOTE_REST,283, NOTE_G5,4, NOTE_G5,595, NOTE_G5,11, NOTE_REST,1788, NOTE_G5,35,
- NOTE_G5,864, NOTE_G5,16, NOTE_REST,283, NOTE_G5,4, NOTE_G5,595, NOTE_G5,11, NOTE_REST,588, NOTE_B4,600,
- NOTE_A4,508, NOTE_A4,91, NOTE_G4,1200, NOTE_G4,600, NOTE_G4,11, NOTE_G4,1188, NOTE_G4,600, NOTE_C5,35,
- NOTE_C5,1164, NOTE_C5,600, NOTE_C5,11, NOTE_C5,1696, NOTE_C5,91, NOTE_F4,1200, NOTE_F4,600, NOTE_F4,11,
- NOTE_F4,888, NOTE_F4,16, NOTE_F4,283, NOTE_F4,4, NOTE_F4,595, NOTE_C5,11, NOTE_C5,888, NOTE_C5,16,
- NOTE_C5,283, NOTE_C5,4, NOTE_C5,595, NOTE_C5,11, NOTE_C5,1696, NOTE_C5,91, NOTE_F4,1200, NOTE_F4,600,
- NOTE_F4,11, NOTE_F4,888, NOTE_F4,16, NOTE_F4,283, NOTE_F4,4, NOTE_F4,595, NOTE_C5,11, NOTE_C5,888,
- NOTE_C5,16, NOTE_C5,283, NOTE_C5,4, NOTE_C5,595, NOTE_C5,11, NOTE_C5,588, NOTE_B4,600, NOTE_A4,508,
- NOTE_A4,91, NOTE_G4,1200, NOTE_G4,600, NOTE_G4,11, NOTE_G4,588, NOTE_G4,11, NOTE_G4,588, NOTE_G4,11,
- NOTE_G4,588, NOTE_C5,11, NOTE_C5,1788, NOTE_C5,35, NOTE_C5,1673, NOTE_C5,91, NOTE_C5,600, NOTE_C5,11,
- NOTE_C5,588, NOTE_C5,11, NOTE_C5,588, NOTE_G4,11, NOTE_G4,888, NOTE_G4,16, NOTE_G4,283, NOTE_G4,4,
- NOTE_G4,595, NOTE_C5,11, NOTE_C5,1696, NOTE_REST,91,
- TONES_END
-};
-
-
-// This function runs once in your game.
-// use it for anything that needs to be set only once in your game.
-void setup() {
-  // initiate arduboy instance
+void setup()
+{
   arduboy.begin();
-
-  // here we set the framerate to 15, we do not need to run at
-  // default 60 and it saves us battery life
-  arduboy.setFrameRate(15);
+  arduboy.setTextSize(4);
+  arduboy.setCursor(0,0);
+  arduboy.print("Music\nDemo");
+  arduboy.display();
 }
 
 
-// our main game loop, this runs once every cycle/frame.
-// this is where our game logic goes.
-void loop() {
+int x = 0, y = 0;
+
+void loop ()
+{
   // pause render until it's time for the next frame
   if (!(arduboy.nextFrame()))
     return;
 
-  // first we clear our screen to black
+  if (arduboy.pressed(UP_BUTTON)) {
+    y-=1;
+  } else if (arduboy.pressed(DOWN_BUTTON)) {
+    y+=1;
+  } else if (arduboy.pressed(LEFT_BUTTON)) {
+    x-=1;
+  } else if (arduboy.pressed(RIGHT_BUTTON)) {
+    x+=1;
+  }
+
+  if (arduboy.pressed(A_BUTTON)) {
+    arduboy.invert(true);
+  } else if (arduboy.pressed(B_BUTTON)) {
+    arduboy.invert(false);
+  }
+
   arduboy.clear();
-
-  // we set our cursor 5 pixels to the right and 10 down from the top
-  // (positions start at 0, 0) 
-  arduboy.setCursor(4, 9);
-
-  // then we print to screen what is in the Quotation marks ""
-  arduboy.print(F("Hello, world!"));
-
-  // then we finaly we tell the arduboy to display what we just wrote to the display
+  arduboy.setCursor(x,y);
+  arduboy.print("Music\nDemo");
   arduboy.display();
 
-  sound.tones(score);
-
+  
+  // play the tune if we aren't already
+  if (!arduboy.tunes.playing()) {
+    arduboy.tunes.tone(329, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(329, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(554, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(554, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(523, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(440, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(440, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(493, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(440, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(440, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(440, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 10.0446328125);
+    delay(11.160703125);
+    arduboy.tunes.tone(783, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(880, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(739, 482.142375);
+    delay(535.71375);
+    arduboy.tunes.tone(659, 80.3570625);
+    delay(89.285625);
+    arduboy.tunes.tone(739, 80.3570625);
+    delay(89.285625);
+    arduboy.tunes.tone(783, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(880, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(987, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(783, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(880, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(739, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(659, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 1285.713);
+    delay(1428.57);
+    arduboy.tunes.tone(783, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(880, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(739, 482.142375);
+    delay(535.71375);
+    arduboy.tunes.tone(659, 80.3570625);
+    delay(89.285625);
+    arduboy.tunes.tone(739, 80.3570625);
+    delay(89.285625);
+    arduboy.tunes.tone(783, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(880, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(987, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(739, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(659, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(739, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(783, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(1174, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(987, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(880, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(880, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(880, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(783, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(698, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(783, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(987, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(987, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(987, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(987, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(783, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(659, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(523, 482.142375);
+    delay(535.71375);
+    arduboy.tunes.tone(523, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(493, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(523, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(440, 482.142375);
+    delay(535.71375);
+    arduboy.tunes.tone(880, 80.3570625);
+    delay(89.285625);
+    arduboy.tunes.tone(880, 80.3570625);
+    delay(89.285625);
+    arduboy.tunes.tone(880, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(659, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(622, 562.4994375);
+    delay(624.999375);
+    delay(89.285625);
+    arduboy.tunes.tone(739, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(1046, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(987, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 723.2135625);
+    delay(803.570625);
+    delay(89.285625);
+    arduboy.tunes.tone(659, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(880, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(987, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(987, 1285.713);
+    delay(1428.57);
+    arduboy.tunes.tone(329, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(329, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(554, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(554, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(329, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(329, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(554, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(554, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(329, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(329, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(554, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(554, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(329, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(329, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(554, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(554, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+    arduboy.tunes.tone(329, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(369, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(391, 321.42825);
+    delay(357.1425);
+    arduboy.tunes.tone(329, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(391, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(587, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(554, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(493, 241.0711875);
+    delay(267.856875);
+    arduboy.tunes.tone(554, 160.714125);
+    delay(178.57125);
+    arduboy.tunes.tone(659, 642.8565);
+    delay(714.285);
+  }
 }
