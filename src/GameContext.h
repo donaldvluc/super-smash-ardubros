229f9474b4d2f0081289d8ab37e4bc84af7f6ca9
Anthony Luc <anthonyvanluc@gmail.com>
2019-01-24 00:13:35 -0500

    init gamecontext

---
+++ src/GameContext.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Idea: maybe use to handle game camera and following across stage/map
+
+
+class GameContext
+{
+private:
+  int m_w;
+  int m_h;
+public:
+  GameContext(void);
+}
